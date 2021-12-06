5b25403d5ac036e03aaaf224d4e27a88fd6e526b
Billal Hossain <billal4b@gmail.com>
2021-12-06 20:48:48 +0600

    calendar app

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<template>
+<AppHeader/>
+
+<div class="w-full flex"> 
+  <Calendar/>
+</div>
+</template>
+
+<script>
+import AppHeader from './components/AppHeader';
+import Calendar from './components/Calendar';
+import './assets/tailwind.css';
+export default {
+  name: 'App',
+  components: { AppHeader, Calendar },
+}
+</script>
+
+
---
+++ src/components/AppHeader.vue
@@ -0,0 +1,25 @@
+<template>
+  <nav class="relative flex flex-wrap items-center justify-between px-2 py-3 bg-green-600 mb-3">
+    <div class="container px-4 mx-auto flex flex-wrap items-center justify-between">
+      <div class="w-full relative flex justify-between lg:w-auto  px-4 lg:static lg:block lg:justify-start">
+        <a class="text-sm font-bold leading-relaxed inline-block mr-4 py-2 whitespace-nowrap uppercase text-white" href="/">
+          Multi App
+        </a>      
+      </div>    
+        <ul class="flex">
+          <li class="nav-item">
+            <a class="px-3 py-2 flex items-center text-xs uppercase font-bold leading-snug text-white hover:opacity-75" href="#">
+              <span class="ml-2">Calendar</span>
+            </a>
+          </li>
+        
+        </ul>      
+    </div>
+  </nav>
+</template>
+
+<script>
+export default {
+  
+}
+</script>
